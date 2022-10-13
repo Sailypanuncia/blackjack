3329cf28feaaf53d630d1421537d405db290e0e2
Saily <sailypanuncia@gmail.com>
2022-10-13 16:11:32 +0200

    Cartas de computadora

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -14,8 +14,10 @@ let puntosJugador = 0;
 
 // Referencias del HTML
 const btnPedir = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
 
-const divCartasJugador = document.querySelector('#jugador-cartas');
+const divCartasJugador     = document.querySelector('#jugador-cartas');
+const divCartasComputadora = document.querySelector('#computadora-cartas');
 const puntosHTML = document.querySelectorAll('small');
 
 //esta funcion crea una nueva baraja, es decir, baraja las cartas
@@ -51,6 +53,26 @@ const valorCarta = (carta)=>{
         : valor * 1;
 }
 
+//turno de la computadora
+const turnoComputadora = (puntosMinimos)=>{
+  do{
+    const carta = pedirCarta();
+    puntosComputadora = puntosComputadora + valorCarta(carta);
+    puntosHTML[1].innerText = puntosComputadora;
+  
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+    divCartasComputadora.append(imgCarta);
+
+    if(puntosMinimos>21){
+      break;
+    }
+
+  } while((puntosComputadora<puntosMinimos) && (puntosMinimos<=21));
+}
+
+
 //Eventos
 btnPedir.addEventListener('click', ()=>{
   const carta = pedirCarta();
@@ -61,15 +83,28 @@ btnPedir.addEventListener('click', ()=>{
   imgCarta.src = `assets/cartas/${carta}.png`;
   imgCarta.classList.add('carta');
   divCartasJugador.append(imgCarta);
-
+  
+  if(puntosJugador>21){
+    console.warn('Lo siento mucho, perdiste');
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora(puntosJugador);
+  }else if(puntosJugador === 21){
+    console.warn('21, genial');
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora(puntosJugador);
+  }
   
 });
 
 
-
-
-
-
+btnDetener.addEventListener('click', ()=>{
+  btnPedir.disabled = true;
+  btnDetener.disabled = true;
+  turnoComputadora(puntosJugador);
+  
+});
 
 
 
